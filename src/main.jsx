2ec48e394fcab120ec870cfc8860b903fb206d70
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2025-02-13 07:20:20 +0100

    Change default to dark mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./components/home/Home";
+import styles from "./main.module.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-      <Home></Home>
-    </React.StrictMode>
-  );
+  <React.StrictMode>
+    <div className={styles.root}>
+      <Home />
+    </div>
+  </React.StrictMode>
+);
