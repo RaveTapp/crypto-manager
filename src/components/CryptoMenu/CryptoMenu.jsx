c902e7d0c27d613308c7d9efe6004395e7fbfab4
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2025-02-13 19:33:39 +0100

    Separate CryptoMenu css

--- src/components/CryptoMenu/CryptoMenu.jsx
+++ src/components/CryptoMenu/CryptoMenu.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import styles from '../home/Home.module.css';
+import React from "react";
+import styles from "./CryptoMenu.module.css";
 
-export default function CryptoMenu({ supportedCryptos, selectedCryptos, toggleSelection }) {
+export default function CryptoMenu({
+  supportedCryptos,
+  selectedCryptos,
+  toggleSelection,
+}) {
   return (
     <div className={styles.cryptoMenu}>
       {supportedCryptos.map((crypto) => (
         <div
           key={crypto.symbol}
-          className={`${styles.menuItem} ${selectedCryptos.includes(crypto.symbol) ? styles.selected : ''}`}
+          className={`${styles.menuItem} ${
+            selectedCryptos.includes(crypto.symbol) ? styles.selected : ""
+          }`}
           onClick={() => toggleSelection(crypto.symbol)}
         >
-          <img src={crypto.logo} alt={crypto.name} className={styles.cryptoLogo} />
+          <img
+            src={crypto.logo}
+            alt={crypto.name}
+            className={styles.cryptoLogo}
+          />
           <span>{crypto.name}</span>
         </div>
       ))}
