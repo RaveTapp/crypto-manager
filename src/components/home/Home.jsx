fa71b14ca83f0fc592bbb0fec8bdcc3dd70ea045
RaveTapp <120050958+RaveTapp@users.noreply.github.com>
2025-02-13 07:21:35 +0100

    Remove unused HoldingsForm

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import styles from "./Home.module.css";
 import PriceList from "../PriceList/PriceList";
-import HoldingsForm from "../HoldingsForm/HoldingsForm";
 import PortfolioShowcase from "../PortfolioShowcase/PortfolioShowcase";
 import CryptoMenu from "../CryptoMenu/CryptoMenu";
-import { supportedCryptos } from "./supportedCryptos";
+import { supportedCryptos } from "./supportedCryptos.js";
 
 function Home() {
   const [selectedCryptos, setSelectedCryptos] = useState(
